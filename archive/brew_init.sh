eafdb5973f5db0b6439a9142f7ed1098e871e6be
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2025-01-26 14:47:06 -0500

    Remove archive dir from git tracking

--- archive/brew_init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-echo "Installing xcode"
-xcode-select --install
-
-echo "Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
